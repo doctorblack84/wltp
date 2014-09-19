2767d14ddbb8721f521cc4579be47d69c93dc5e6
Kostis Anagnostopoulos @ STUW025 <ankostis@gmail.com>
2014-09-20 00:37:48 +0200

    docs: FIX3(!) typo in RTD-mocks. [ci skip]

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ if on_rtd:
                 return Mock()
 
     MOCK_MODULES = [
-        'jsonschema', 'jsonschema.exceptions'
+        'jsonschema', 'jsonschema.exceptions',
         'numpy', 'numpy.testing',
         'pandas', 'pandas.core', 'pandas.core.generic', 'pandas.core.common',
         'scipy',
